cbc3ce1e63138c755d95547681f1bf4aa301141a
Catalin Glavan <glmihaicata@gmail.com>
2025-11-25 23:59:44 +0200

    upgrade next version and node

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import {i18nRouter} from 'next-i18n-router';
 import {NextRequest} from 'next/server';
 import i18nConfig from '@/config/i18n';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     return i18nRouter(request, i18nConfig);
 }
 
